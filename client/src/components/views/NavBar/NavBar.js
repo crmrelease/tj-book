a0d72e93adc2c7367388a658173938b8d2bf7039
ktjin2003 <ktjin2003@gmail.com>
2020-03-23 01:20:44 +0900

    사소한 수정

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import LeftMenu from './sections/LeftMenu';
 import RightMenu from './sections/RightMenu';
 import { Drawer, Button, icon } from 'antd';
 import './sections/NavBar.css';
+const logo = require('../../../asset/logo.png')
 
 //const logo = require('../../../assets/youtube_logo_download_sns.png')
 
@@ -20,7 +21,7 @@ function NavBar() {
     return (
       <nav className="menu" style={{ position: 'fixed', zIndex: 1, width: '100%', height:'10%' }}>
         <div className="menu__logo">
-          <a href="/" >로고<img src style={{width:'150%', marginTop:'-30px'}}/></a>
+          <a href="/" ><img src={logo} style={{width:'90%'}}/></a>
         </div>
         <div className="menu__container">
           <div className="menu_left">
